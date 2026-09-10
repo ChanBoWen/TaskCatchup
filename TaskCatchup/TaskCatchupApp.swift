88c9675b3981af2e04d8aee138917c1c2633941e
ChanBoWen <bowenchan0528@gmail.com>
2026-09-10 17:41:02 +1000

    Initial Commit

---
+++ TaskCatchup/TaskCatchupApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskCatchupApp.swift
+//  TaskCatchup
+//
+//  Created by Bo Wen Chan on 10/9/2026.
+//
+
+import SwiftUI
+
+@main
+struct TaskCatchupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TaskCatchupTests/TaskCatchupTests.swift
@@ -0,0 +1,19 @@
+//
+//  TaskCatchupTests.swift
+//  TaskCatchupTests
+//
+//  Created by Bo Wen Chan on 10/9/2026.
+//
+
+import Testing
+@testable import TaskCatchup
+
+struct TaskCatchupTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
